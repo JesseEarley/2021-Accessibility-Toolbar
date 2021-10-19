99e230fdd346692106e22404fdf21a4b669787a2
Jesse <jesse.earley@gmail.com>
2021-10-19 13:29:25 -0400

    Eliminated almost 60 lines of code by refactoring the addEventListeners on the toggle and setting the cookies by breaking this out into two functions and looping

--- js/accessibilitytoolbar.js
+++ js/accessibilitytoolbar.js
@@ -7,7 +7,7 @@ function isTouchDevice() {
 /* This function will set a cookie */
 function setCookie(key, value, path, time) {
     if (time != '') {
-        var expires = new Date();
+        let expires = new Date();
         expires.setTime(expires.getTime() + time);
         document.cookie = key + '=' + value + ';path=' + path + ';expires=' + expires.toUTCString() + ';sameSite=Lax';
     }
@@ -19,7 +19,7 @@ function setCookie(key, value, path, time) {
 
 /* This function will return a cookie */
 function getCookie(key) {
-    var keyValue = document.cookie.match('(^|;) ?' + key + '=([^;]*)(;|$)');
+    let keyValue = document.cookie.match('(^|;) ?' + key + '=([^;]*)(;|$)');
     return keyValue ? keyValue[2] : null;
 }  
 
@@ -30,13 +30,13 @@ function toggleAccessibilityTools(){
 
 /* This function is called on page load to see if settings need to be pulled from cookies. If they do, those classes are toggled. */
 function checkAccessibiliy(){
-	var accessibilityGrayscale = getCookie('accessibility_grayscale');
-	var accessibilityHighlightLinks = getCookie('accessibility_highlightLinks');
-	var accessibilityContrast = getCookie('accessibility_contrast');
-	var accessibilityTextSize = getCookie('accessibility_textSize');
-	var accessibilityletterSpacing = getCookie('accessibility_letterSpacing');
-	var accessibilityDyslexiaFriendly = getCookie('accessibility_dyslexiaFriendly');
-	var accessibilityCursorSize = getCookie('accessibility_cursorSize');
+	let accessibilityGrayscale = getCookie('accessibility-grayscale');
+	let accessibilityHighlightLinks = getCookie('accessibility-highlightLinks');
+	let accessibilityContrast = getCookie('accessibility-contrast');
+	let accessibilityTextSize = getCookie('accessibility-textSize');
+	let accessibilityletterSpacing = getCookie('accessibility-letterSpacing');
+	let accessibilityDyslexiaFriendly = getCookie('accessibility-dyslexiaFriendly');
+	let accessibilityCursorSize = getCookie('accessibility-cursorSize');
 
 	if (accessibilityGrayscale == 'true')
 	{
@@ -74,7 +74,7 @@ function checkAccessibiliy(){
 		document.querySelector('#AccessibilityToolsCursorSize').checked = true;
 	}
 
-	/*If on mobile, we need to display the accessibility button*/
+	/* If on mobile, we need to display the accessibility button */
 	if (isTouchDevice()) {
 		document.querySelector(
 			"#AccessibilityToolsButton-Mobile"
@@ -82,12 +82,33 @@ function checkAccessibiliy(){
 	}
 }
 
-var ready = (callback) => {
-    if (document.readyState != "loading") callback();
-    else document.addEventListener("DOMContentLoaded", callback);
-  }
+/* This function is called when a toggle is clicked. It will toggle a class on the HTML element and set a cookie */
+function clickAccessibilityToggle(evt){
+	console.log(evt.currentTarget);
+	document.querySelector("html").classList.toggle(evt.currentTarget.dataset.cookieandclass);
+	if (document.querySelector('html').classList.contains(evt.currentTarget.dataset.cookieandclass))
+	{
+		setCookie(evt.currentTarget.dataset.cookieandclass, 'true', '/','31 Dec 9999 23:59:59 GMT');
+	}
+	else{
+		setCookie(evt.currentTarget.dataset.cookieandclass, 'false', '/','');
+	}	
+}
 
-ready(() => { 
+/* This function is called on page load and adds Event Listeners to all of the toggles in the Accessibility Toolbar */
+function addToggleEventListeners(accessibilityToggles){
+	for(i = 0; i < accessibilityToggles.length; i++){
+		document.querySelector("#" + accessibilityToggles[i].id).addEventListener('click',clickAccessibilityToggle);
+	}
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+	/* Get all of the toggles in the Accessibility Toolbar */
+	let accessibilityToggles = document.getElementsByClassName('accessibilityToggles');
+
+	/* Add Event Listerners to the toggles */
+	addToggleEventListeners(accessibilityToggles);
+	
 	/* Need to check to see if we need to display the accessibility tool settings */
 	checkAccessibiliy();
  
@@ -98,85 +119,7 @@ ready(() => {
         }
     }
 
-	/* Need to add event listeners to the accessibility tool buttons. When clicked, a class will be toggled and a cookie will be set. The cookie will,
-	  ensure that our settings are saved the next time we visit the site or a pate within the site. */
-	document.querySelector('#AccessibilityToolsGrayscale').addEventListener('click',function(){
-	    document.querySelector("html").classList.toggle("accessibilityTools-grayscale");
-		if (document.querySelector('html').classList.contains('accessibilityTools-grayscale'))
-		{
-			setCookie('accessibility_grayscale', 'true', '/','31 Dec 9999 23:59:59 GMT');
-		}
-		else{
-			setCookie('accessibility_grayscale', 'false', '/','');
-		}	
-	});
-
-	document.querySelector('#AccessibilityToolsHightLinks').addEventListener('click',function(){
-		document.querySelector("html").classList.toggle("accessibilityTools-highlightLinks");
-		if (document.querySelector('html').classList.contains('accessibilityTools-highlightLinks'))
-		{
-			setCookie('accessibility_highlightLinks', 'true', '/','31 Dec 9999 23:59:59 GMT');
-		}
-		else{
-			setCookie('accessibility_highlightLinks', 'false', '/','');
-		}	
-	});
-
-	document.querySelector('#AccessibilityToolsContrast').addEventListener('click',function(){
-		document.querySelector("html").classList.toggle("accessibilityTools-contrast");
-		if (document.querySelector('html').classList.contains('accessibilityTools-contrast'))
-		{
-			setCookie('accessibility_contrast', 'true', '/','31 Dec 9999 23:59:59 GMT');
-		}
-		else{
-			setCookie('accessibility_contrast', 'false', '/','');
-		}	
-	});
-
-	document.querySelector('#AccessibilityToolsTextSize').addEventListener('click',function(){
-		document.querySelector("html").classList.toggle("accessibilityTools-textSize");
-		if (document.querySelector('html').classList.contains('accessibilityTools-textSize'))
-		{
-			setCookie('accessibility_textSize', 'true', '/','31 Dec 9999 23:59:59 GMT');
-		}
-		else{
-			setCookie('accessibility_textSize', 'false', '/','');
-		}	
-	});
-
-	document.querySelector('#AccessibilityToolsLetterSpacing').addEventListener('click',function(){
-		document.querySelector("html").classList.toggle("accessibilityTools-letterSpacing");
-		if (document.querySelector('html').classList.contains('accessibilityTools-letterSpacing'))
-		{
-			setCookie('accessibility_letterSpacing', 'true', '/','31 Dec 9999 23:59:59 GMT');
-		}
-		else{
-			setCookie('accessibility_letterSpacing', 'false', '/','');
-		}	
-	});
-
-	document.querySelector('#AccessibilityToolsDyslexiaFriendlyFont').addEventListener('click',function(){
-		document.querySelector("html").classList.toggle("accessibilityTools-dyslexiaFriendly");
-		if (document.querySelector('html').classList.contains('accessibilityTools-dyslexiaFriendly'))
-		{
-			setCookie('accessibility_dyslexiaFriendly', 'true', '/','31 Dec 9999 23:59:59 GMT');
-		}
-		else{
-			setCookie('accessibility_dyslexiaFriendly', 'false', '/','');
-		}	
-	});
-
-	document.querySelector('#AccessibilityToolsCursorSize').addEventListener('click',function(){
-		document.querySelector("html").classList.toggle("accessibilityTools-cursorSize");
-		if (document.querySelector('html').classList.contains('accessibilityTools-cursorSize'))
-		{
-			setCookie('accessibility_cursorSize', 'true', '/','31 Dec 9999 23:59:59 GMT');
-		}
-		else{
-			setCookie('accessibility_cursorSize', 'false', '/','');
-		}	
-	});
-
+/* Add Event Lister on the close button to toggle the Accessibility Toolbar closed */
 	document.querySelector('#AccessibilityToolsButtonClose').addEventListener('click',function(){
 		toggleAccessibilityTools();
 	});
